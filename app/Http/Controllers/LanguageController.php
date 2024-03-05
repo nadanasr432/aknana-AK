4733b57a688b9aab9925143eae871fbef7f79595
nadanasr432 <nadanm432@gmail.com>
2024-03-05 16:39:07 +0200

    add images lang

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
 
 class LanguageController extends Controller
 {
-    public function switch(Request $request)
-    {
-        $request->validate([
-            'locale' => ['required', 'in:en,ar'],
-        ]);
+   // LanguageController.php
+public function switch(Request $request)
+{
+    $request->validate([
+        'locale' => ['required', 'in:en,ar'],
+    ]);
 
-        session(['locale' => $request->locale]);
+    session(['locale' => $request->locale]);
 
-        return redirect()->back();
-    }
+    return redirect()->back();
+}
+
 }
